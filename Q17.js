ec2c18fe826eff8b4f57ac01ae8f57811602926c
Vikas <tondev23@gmail.com>
2022-05-03 09:19:12 +0530

    Q16 Done

---
+++ Q17.js
@@ -0,0 +1,11 @@
+var re = /^\d{10}$/;
+function testInfo(phoneInput) {
+    var OK = re.exec(phoneInput);
+    if (!OK) {
+        console.error(phoneInput + ' Phone number should be 10 digits long');
+    } else {
+        console.log('Thanks, your phone number is ' + OK[0]);
+    }
+}
+
+testInfo("8308859743");
