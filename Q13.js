1253f60ed81f9b920833a6b26f950e90c5b24596
Vikas <tondev23@gmail.com>
2022-05-03 08:57:39 +0530

    Q13 Done

--- Q13.js
+++ Q13.js
@@ -1,18 +1,18 @@
 //  Implement linear search (Brute Force)
 
-var arr= [2,6,3,8,9,12,54,4];
-var key= 12;
+var arr = [2, 6, 3, 8, 9, 12, 54, 4];
+var key = 12;
 var flag = false;
-var i=0;
-for(i=0;i<arr.length;i++){
-    if(arr[i]==key){
-        flag=true;
+var i = 0;
+for (i = 0; i < arr.length; i++) {
+    if (arr[i] == key) {
+        flag = true;
         break;
     }
 }
-if(flag){
-    console.log(`Key ${key} is found at position ${i+1}`);
+if (flag) {
+    console.log(`Key ${key} is found at position ${i + 1}`);
 }
-else{   
+else {
     console.log(`Key ${key} is not present in an array`);
 }
